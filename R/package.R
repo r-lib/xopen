e77219338357c2368150b5432a5591daaa315fe2
Gábor Csárdi <csardi.gabor@gmail.com>
2018-09-10 09:20:36 +0100

    First implementation

--- R/package.R
+++ R/package.R
@@ -1,9 +1,37 @@
 
-#' Open System Files, URLs, Anything
-#'
-#' Cross platform solution to open files, directories or URLs with their
-#' associated programs.
-#'
-#' @docType package
-#' @name xopen
-NULL
+#' @export
+
+xopen <- function(target = NULL, app = NULL, app_args = NULL) {
+
+  if (is.null(app) && !is.null(app_args)) {
+    warning("No `app`, so `app_args` are ignored")
+  }
+
+  if (.Platform$OS.type == "windows") {
+    cmd <- "cmd"
+    args <- c("/c", "start", "\"\"", "/b")
+    target <- gsub("&", "^&", target)
+    if (!is.null(app)) args <- c(args, app, app_args)
+    args <- c(args, target)
+
+  } else if (Sys.info()[["sysname"]] == "Darwin") {
+    cmd <- "open"
+    args <- if (!is.null(app)) c("-a", app)
+    args <- c(args, target)
+    if (!is.null(app)) args <- c(args, "--args",  app_args)
+
+  } else {
+    if (!is.null(app)) {
+      cmd <- app
+      args <- app_args
+    } else  {
+      cmd <- system.file("xdg-open", package = "xopen")
+      args <- character()
+    }
+    args <- c(args, target)
+  }
+
+  px <- processx::process$new(cmd, as.character(args))
+
+  invisible(px)
+}
