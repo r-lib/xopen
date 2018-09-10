cb7306df125cf25d078c477131a95bfc61db473b
Gábor Csárdi <csardi.gabor@gmail.com>
2018-09-10 11:18:58 +0100

    REfactoring

--- R/package.R
+++ R/package.R
@@ -1,38 +1,57 @@
 
 #' @export
 
-xopen <- function(target = NULL, app = NULL, app_args = NULL) {
+xopen <- function(target = NULL, app = NULL, app_args = NULL,
+                  quiet = FALSE) {
 
   if (is.null(app) && !is.null(app_args)) {
     warning("No `app`, so `app_args` are ignored")
   }
 
   if (.Platform$OS.type == "windows") {
-    cmd <- "cmd"
-    args <- c("/c", "start", "\"\"", "/b")
-    target <- gsub("&", "^&", target)
-    if (!is.null(app)) args <- c(args, app, app_args)
-    args <- c(args, target)
+    par <- xopen_win(target, app, app_args)
 
   } else if (Sys.info()[["sysname"]] == "Darwin") {
-    cmd <- "open"
-    args <- if (!is.null(app)) c("-a", app)
-    args <- c(args, target)
-    if (!is.null(app)) args <- c(args, "--args",  app_args)
+    par <- xopen_macos(target, app, app_args)
 
   } else {
-    if (!is.null(app)) {
-      cmd <- app
-      args <- app_args
-    } else  {
-      cmd <- Sys.which("xdg-open")
-      if (cmd == "") cmd <- system.file("xdg-open", package = "xopen")
-      args <- character()
-    }
-    args <- c(args, target)
+    par <- xopen_other(target, app, app_args)
   }
 
-  px <- processx::process$new(cmd, as.character(args))
+  err <- tempfile()
+  on.exit(unlink(err, recursive = TRUE), add = TRUE)
+  px <- processx::process$new(par[[1]], par[[2]], stderr = err,
+                              echo_cmd = !quiet)
 
   invisible(px)
+}
+
+xopen_macos <- function(target, app, app_args) {
+  cmd <- "open"
+  args <- if (!is.null(app)) c("-a", app)
+  args <- c(args, target)
+  if (!is.null(app)) args <- c(args, "--args",  app_args)
+  list(cmd, args)
+}
+
+xopen_win <- function(target, app, app_args) {
+  cmd <- "cmd"
+  args <- c("/c", "start", "\"\"", "/b")
+  target <- gsub("&", "^&", target)
+  if (!is.null(app)) args <- c(args, app, app_args)
+  args <- c(args, target)
+  list(cmd, args)
+}
+
+xopen_other <- function(taget, app, app_args) {
+  if (!is.null(app)) {
+    cmd <- app
+    args <- app_args
+  } else  {
+    cmd <- Sys.which("xdg-open")
+    if (cmd == "") cmd <- system.file("xdg-open", package = "xopen")
+    args <- character()
+  }
+  args <- c(args, target)
+  list(cmd, args)
 }
