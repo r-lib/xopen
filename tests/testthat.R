c59ee7bf56539e04054d37668e0d2c05df4792cf
Gábor Csárdi <csardi.gabor@gmail.com>
2018-09-10 13:22:18 +0100

    Use the cleanup reporter from ps

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,11 @@
 library(testthat)
 library(xopen)
 
-test_check("xopen")
+if (ps::ps_is_supported()) {
+  reporter <- ps::CleanupReporter(testthat::SummaryReporter)$new()
+} else {
+  ## ps does not support this platform
+  reporter <- "progress"
+}
+
+test_check("xopen", reporter = reporter)
